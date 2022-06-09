0a89d8a705d51d16ea4c00e341dd7a72beda67d9
thaimynguyen <thaimynguyen@gmail.com>
2022-06-09 12:09:37 -0700

    Add text color change function for temp

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 'use strict';
-let currentTemp = parseInt(document.getElementById('current-temp').textContent);
+const state = {
+  currentTemp: 70,
+};
+
+const currentTempElement = document.getElementById('current-temp');
+
+const getCurrentTemp = () =>
+  parseInt(document.getElementById('current-temp').textContent);
+
 const increaseTemp = () => {
-  currentTemp += 1;
-  document.getElementById('current-temp').textContent = `${currentTemp}`;
+  state.currentTemp += 1;
+  document.getElementById('current-temp').textContent = `${state.currentTemp}`;
 };
 
 const decreaseTemp = () => {
-  currentTemp -= 1;
-  document.getElementById('current-temp').textContent = `${currentTemp}`;
+  state.currentTemp -= 1;
+  document.getElementById('current-temp').textContent = `${state.currentTemp}`;
 };
 
+const weatherGarden = document.getElementById('weather-garden');
+
 const registerEventHandlers = () => {
-  const currentTemp = document.getElementById('current-temp');
   const increaseButton = document.getElementById('increase-button');
   const decreaseButton = document.getElementById('decrease-button');
   increaseButton.addEventListener('click', increaseTemp);
   decreaseButton.addEventListener('click', decreaseTemp);
-  // currentTemp.textContent = currentTemp.value;
+  console.log(state.currentTemp);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
