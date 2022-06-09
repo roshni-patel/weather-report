66cb3da2421feb5f1df899cf1ceb26c15bdf28d8
Roshni Patel <rpatel01@wesleyan.edu>
2022-06-09 11:37:27 -0700

    Add event listener for increase and decrease buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 'use strict';
+let currentTemp = parseInt(document.getElementById('current-temp').textContent);
+const increaseTemp = () => {
+  currentTemp += 1;
+  document.getElementById('current-temp').textContent = `${currentTemp}`;
+};
+
+const decreaseTemp = () => {
+  currentTemp -= 1;
+  document.getElementById('current-temp').textContent = `${currentTemp}`;
+};
+
+const registerEventHandlers = () => {
+  const currentTemp = document.getElementById('current-temp');
+  const increaseButton = document.getElementById('increase-button');
+  const decreaseButton = document.getElementById('decrease-button');
+  increaseButton.addEventListener('click', increaseTemp);
+  decreaseButton.addEventListener('click', decreaseTemp);
+  // currentTemp.textContent = currentTemp.value;
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
